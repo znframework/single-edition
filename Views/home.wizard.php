dcd5b55a76621ee69c62943ff790ebb030d2787b
ozanbote <ozanbote@windowslive.com>
2017-06-13 09:35:32 +0300

    5.0.0 -> Updated Open View
    5.0.0 -> Updated Open View

--- Views/home.wizard.php
+++ Views/home.wizard.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>ZN FRAMEWORK SINGLE EDITION</title>
+        <title>{{$title}}</title>
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <style>
             html, body{height: 100%;}
@@ -18,8 +18,8 @@
     <body>
     	<div id="container">
             <div id="content">
-                <div id="title">ZN FRAMEWORK</div>
-                <div id="sub-title">Single Edition</div>
+                <div id="title">{{$title}}</div>
+                <div id="sub-title">{{$subtitle}}</div>
             </div>
         </div>
     </body>
