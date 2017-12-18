d6d12e1a409ddd1cec9959164db1da94ca83bdb7
Ozan UYKUN <znframework@gmail.com>
2017-12-18 22:18:16 +0300

    Updated Open Page
    Updated Open Page

--- Controllers/home.php
+++ Controllers/home.php
@@ -5,8 +5,8 @@ class Home extends Controller
     public function main(String $params = NULL)
     {
         // Simplicity is our choice, how about yours ?
-
-        $this->view->title    = 'ZN5 ORIGINAL';
-        $this->view->subtitle = 'Community Edition';
+        
+        View::pageTitle('ZN FRAMEWORK')
+            ->pageSubtitle('"Simplicity is the ultimate sophistication" - Da Vinci');
     }
 }
