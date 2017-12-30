a691fdd0816e2af717116570a5bf662a302f3fd1
Ozan UYKUN <znframework@gmail.com>
2017-12-30 10:36:56 +0300

    5.5 -> Updated Version
    ZN 5.5 is dedicated to Nikola Tesla
    "Simplicity is the ultimate sophistication" - Da Vinci

--- Config/Hooks.php
+++ Config/Hooks.php
@@ -8,11 +8,11 @@
     | You can use the Hook class to add functions to this file.
     | Expressions used as key are used as method name in the Hook class.
     |
-    | Example: Hook::project('example'); // example
+    | Example: Hook::general('example'); // example
     |
     */
-   
-    'project' => function(String $name)
+
+    'general' => function(String $name)
     {
         return $name;
     }
