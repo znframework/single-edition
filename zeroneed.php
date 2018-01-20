e556810779193c518f8882ab2427595610d58f51
Ozan UYKUN <znframework@gmail.com>
2018-01-20 12:56:10 +0300

    Updated Single Edition

--- zeroneed.php
+++ zeroneed.php
@@ -19,7 +19,7 @@
 |
 */
 
-require_once __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
